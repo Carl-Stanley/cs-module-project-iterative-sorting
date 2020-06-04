d95c7f2cc7602d3dad8dd5e892b4a001d6bd7d89
Carl-Stanley <cds.beml@gmail.com>
2020-06-03 19:32:16 -0700

    COmmit

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,25 @@
 def linear_search(arr, target):
     # Your code here
+    for x in range(len(arr)):        
+        if arr[x] == target:
+            return x
 
-
-    return -1   # not found
+    return -1    
 
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
+def binary_search(arr, target):     
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right)
+         
+        if arr[mid] == target:
+            return mid        
+        
+        if arr[mid] < target:            
+            left = mid + 1         
+        else:             
+            right = mid - 1
+    
+    return -1 
